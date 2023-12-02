57abe339f14178aa3609bd5bf6194232f727ba4a
Ishan465 <patelishan691@gmail.com>
2023-12-02 17:01:48 -0500

    test

--- tests/test_keycard_monotone.py
+++ tests/test_keycard_monotone.py
@@ -7,39 +7,43 @@ from src.keycard import Keycard
 def test_monotonic_keycard():
     """This will check whether the function is monotonic"""
     assert is_monotone(
-        Keycard([-1, -5, -10, -1100, -1100, -1101, -1102, -9001])
+        Keycard([-1, -5, -10, -1100, -1100, -1101, -1102, -9001], any)
     ), "Test Failed: Expected is monotone"
 
-    assert is_monotone(Keycard([1])), "Test Failed: Expected is monotone"
-
-    assert is_monotone(Keycard([1, 2])), "Test Failed: Expected is monotone"
-
-    assert is_monotone(Keycard([2, 1])), "Test Failed: Expected is monotone"
+    assert is_monotone(Keycard([1], any)), "Test Failed: Expected is monotone"
 
     assert is_monotone(
-        Keycard([1, 5, 10, 1100, 1101, 1102, 9001])
+        Keycard([1, 2], any)
+    ), "Test Failed: Expected is monotone"
+
+    assert is_monotone(
+        Keycard([2, 1], any)
+    ), "Test Failed: Expected is monotone"
+
+    assert is_monotone(
+        Keycard([1, 5, 10, 1100, 1101, 1102, 9001], any)
     ), "Test Failed: Expected is monotone"
 
     assert not is_monotone(
-        Keycard([-1, -5, -10, -1100, -900, -1101, -1102])
+        Keycard([-1, -5, -10, -1100, -900, -1101, -1102], any)
     ), "Test Failed: Expected not monotone"
 
     assert not is_monotone(
-        Keycard([1, 2, 0, 0, 0])
+        Keycard([1, 2, 0, 0, 0], any)
     ), "Test Failed: Expected not monotone"
 
     assert not is_monotone(
-        Keycard([1, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 7, 9, 10])
+        Keycard([1, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 7, 9, 10], any)
     ), "Test Failed: Expected not monotone"
 
     assert is_monotone(
-        Keycard([1, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 11])
+        Keycard([1, 1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 11], any)
     ), "Test Failed: Expected is monotone"
 
     assert not is_monotone(
-        Keycard([1, 2, -1, -2, -5])
+        Keycard([1, 2, -1, -2, -5], any)
     ), "Test Failed: Expected not monotone"
 
     assert not is_monotone(
-        Keycard([1, 1, 1, 2, 3, 4, 1])
+        Keycard([1, 1, 1, 2, 3, 4, 1], any)
     ), "Test Failed: Expected not monotone"
