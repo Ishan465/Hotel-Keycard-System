f171072c8f5bec5b4ed87116822a69e5213a34e3
Ishan465 <patelishan691@gmail.com>
2023-12-03 16:38:17 -0500

    test

--- tests/test_keycard_contains.py
+++ tests/test_keycard_contains.py
@@ -7,35 +7,35 @@ from src.keycard import Keycard
 def test_keycard_contains():
     """This functions checks whether the input contains secret number or not"""
     assert contains_secret_num(
-        Keycard([3, 5, -4, 8, 11, 1, -1, 6], any), 10
+        Keycard([3, 5, -4, 8, 11, 1, -1, 6]), 10
     ), "Test Failed: Expected contains secret num"
 
     assert contains_secret_num(
-        Keycard([4, 6], any), 10
+        Keycard([4, 6]), 10
     ), "Test Failed: Expected contains secret num"
 
     assert contains_secret_num(
-        Keycard([4, 6, 1, -3], any), 3
+        Keycard([4, 6, 1, -3]), 3
     ), "Test Failed: Expected contains secret num"
 
     assert contains_secret_num(
-        Keycard([-21, 301, 12, 4, 65, 56, 210, 356, 9, -47], any),
+        Keycard([-21, 301, 12, 4, 65, 56, 210, 356, 9, -47]),
         163,
     ), "Test Failed: Expected contains secret num"
 
     assert not contains_secret_num(
-        Keycard([15], any), 15
+        Keycard([15]), 15
     ), "Test Failed: Expected does not contain secret num"
 
     assert not contains_secret_num(
-        Keycard([15], any), 14
+        Keycard([15]), 14
     ), "Test Failed: Expected does not contain secret num"
 
     assert not contains_secret_num(
-        Keycard([3, 5, -4, 8, 11, 1, -1, 6], any), 15
+        Keycard([3, 5, -4, 8, 11, 1, -1, 6]), 15
     ), "Test Failed: Expected does not contain secret num"
 
     assert not contains_secret_num(
-        Keycard([-21, 301, 12, 4, 65, 56, 210, 356, 9, -47], any),
+        Keycard([-21, 301, 12, 4, 65, 56, 210, 356, 9, -47]),
         164,
     ), "Test Failed: Expected does not contain secret num"
